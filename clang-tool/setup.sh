4a9cfe7f420f0057b537a01262dffa7abe1801a3
Aleksei <alexey.kashuba97@gmail.com>
2021-01-07 02:01:04 +0300

    update setup script

--- clang-tool/setup.sh
+++ clang-tool/setup.sh
@@ -1,12 +1,25 @@
+set -x
 apt-get update
 apt-get install software-properties-common git wget -y
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main"
 apt-get update
-apt-get install -y cmake
+apt-get install -y cmake zsh g++ python3-pip
+pip3 install -r ../DBx1000/requirements.txt
 apt-get install -y clang-11 clang-tools-11 clang-11-doc libclang-common-11-dev libclang-11-dev libclang1-11 clang-format-11 clangd-11
 apt-get install -y libllvm11 llvm-11 llvm-11-dev llvm-11-doc llvm-11-examples llvm-11-runtime
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-11 20
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-11 20
 update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-11 20
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd .. 
+pwd
+export PROJECT_PATH=$(pwd) 
+cd DBx1000 
+pwd
+export BUILD_DIR=$(pwd) 
+cd ../clang-tool
+echo $PROJECT_PATH
+sh -c ./gen_compile_commands.sh > ../compile_commands.json
 
